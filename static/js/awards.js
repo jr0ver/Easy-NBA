79b54770196c0f2c959b02dfb864228b2f47d384
zohair-syed-uoft <zohairr.syed@mail.utoronto.ca>
2024-10-30 22:34:04 -0400

    feat: redesigned frontend

---
+++ static/js/awards.js
@@ -0,0 +1,36 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // get the awards data from the data attribute
+    const awardsContainer = document.getElementsByClassName('awards')[0];
+    const awardsData = awardsContainer.getAttribute('data-awards'); // Get the JSON string
+
+    let awards;
+    try {
+        awards = JSON.parse(awardsData);
+    } catch (error) {
+        console.error('Error parsing awards data:', error);
+        return;
+    }
+
+    if (Array.isArray(awards)) {
+        awards.forEach(award => {
+            const banner = document.createElement('div'); 
+            banner.className = 'award';
+
+            // svg for icons?
+            // const svg = `<svg width="24" height="24" xmlns="http://www.w3.org/2000/svg">
+            //                 <circle cx="12" cy="12" r="10" fill="#007BFF" />
+            //              </svg>`; // Example SVG (blue circle)
+
+            const textElement = document.createElement('p');
+            textElement.innerText = `${award.name} (${award.year})`;
+            textElement.className = 'award-text';
+
+            // banner.innerHTML = svg;
+            banner.appendChild(textElement);
+
+            awardsContainer.appendChild(banner); // add the banner to the awards container
+        });
+    } else {
+        console.error('Awards data is not an array or is undefined');
+    }
+});
