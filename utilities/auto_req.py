d304dbcb705d217514791d5ec236adc76d11b61c
Zohair Syed <1syedzohair@gmail.com>
2024-12-15 20:47:36 -0500

    feat: base KMeans model

--- utilities/auto_req.py
+++ utilities/auto_req.py
@@ -2,27 +2,19 @@ import random
 from time import sleep
 import requests
 
-# manual for now
-names = ["hakeem olajuwon", "andre iguodala",
-    "hasheem thabeet", "luc longley", "ja morant", "zion williamson", "chris paul",
-    "kevin mchale", "scottie pippen", "trae young", "julius erving", "jalen brunson",
-    "karl-anthony towns", "goran dragić", "devin booker", "chris bosh", "muggsy bogues",
-    "antawn jamison", "guerschon yabusele", "al horford", "nik stauskas", "norman powell",
-    "erick dampier", "t.j. warren", "herbert jones", "mark williams", "marvin williams",
-    "james wiseman", "anthony bennett", "jj redick", "kyle korver", "pete maravich",
-    "carmelo anthony", "nate thurmond", "jarrett allen", "andrew bynum", "jason terry",
-    "elgin baylor", "t.j. leaf", "darius garland", "marvin bagley", "bob mcadoo",
-    "gilbert arenas", "yao ming", "paul george", "donovan mitchell", "kyle lowry",
-    "amar'e stoudemire", "anthony davis", "cj mccollum", "zach randolph", "jalen rose",
-    "jabari parker", "hal greer", "malik monk", "bruce brown", "bruce bowen",
-    "jamychal green", "norm nixon", "george hill", "larry bird", "larry nance", "wilt chamberlain"
-]
+file = "all_players_list.txt"
+
+with open(file, "r") as file:
+    names = file.readlines()
+
+names = [name.rstrip("\n") for name in names]
+# print(names)
 
 url = "http://127.0.0.1:5000/"
 
 # send POST requests
 for name in names:
-    delay_time = random.uniform(7, 13)
+    delay_time = random.uniform(5, 7)
     sleep(delay_time)
 
     response = requests.post(url, data={"player": name})
